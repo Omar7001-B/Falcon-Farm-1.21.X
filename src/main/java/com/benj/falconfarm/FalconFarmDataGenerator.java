67306e8b4fed417304b99594b4f7d857b4f3a349
Omar Abbas <Omar.20375710@compit.aun.edu.eg>
2024-08-25 22:57:42 +0300

    Initial commit

---
+++ src/main/java/com/benj/falconfarm/FalconFarmDataGenerator.java
@@ -0,0 +1,11 @@
+package com.benj.falconfarm;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FalconFarmDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
